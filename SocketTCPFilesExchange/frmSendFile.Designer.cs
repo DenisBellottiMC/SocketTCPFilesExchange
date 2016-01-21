d7c81c67d923985d3a440593f5a1a8d6b34500a8
DenisBellottiMC <denis.bellotti@mcengineering.eu>
2016-01-21 12:07:04 +0100

    First Stable Version of SocketTCPFilesExchange

---
+++ SocketTCPFilesExchange/frmSendFile.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SocketTCPFilesExchange
+{
+    partial class frmSendFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.lblSelectFile = new System.Windows.Forms.Label();
+            this.lblIPAddress = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtIPAddress = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(156, 52);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Browse";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            // 
+            // lblSelectFile
+            // 
+            this.lblSelectFile.AutoSize = true;
+            this.lblSelectFile.Location = new System.Drawing.Point(79, 57);
+            this.lblSelectFile.Name = "lblSelectFile";
+            this.lblSelectFile.Size = new System.Drawing.Size(71, 13);
+            this.lblSelectFile.TabIndex = 1;
+            this.lblSelectFile.Text = "Select the file";
+            // 
+            // lblIPAddress
+            // 
+            this.lblIPAddress.AutoSize = true;
+            this.lblIPAddress.Location = new System.Drawing.Point(26, 104);
+            this.lblIPAddress.Name = "lblIPAddress";
+            this.lblIPAddress.Size = new System.Drawing.Size(58, 13);
+            this.lblIPAddress.TabIndex = 2;
+            this.lblIPAddress.Text = "IP Address";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(48, 133);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(36, 13);
+            this.lblPort.TabIndex = 3;
+            this.lblPort.Text = "Port #";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(90, 130);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(141, 20);
+            this.txtPort.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(106, 226);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send File";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtIPAddress
+            // 
+            this.txtIPAddress.Location = new System.Drawing.Point(90, 101);
+            this.txtIPAddress.Name = "txtIPAddress";
+            this.txtIPAddress.Size = new System.Drawing.Size(141, 20);
+            this.txtIPAddress.TabIndex = 7;
+            // 
+            // frmSendFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtIPAddress);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblIPAddress);
+            this.Controls.Add(this.lblSelectFile);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "frmSendFile";
+            this.Text = "Send File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Label lblSelectFile;
+        private System.Windows.Forms.Label lblIPAddress;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.MaskedTextBox txtIPAddress;
+    }
+}
+
---
+++ SocketTCPFilesExchange/frmSendFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocketTCPFilesExchange
+{
+    public partial class frmSendFile : Form
+    {
+        public frmSendFile()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            IPAddress ipAddress;
+            if (IPAddress.TryParse(txtIPAddress.Text, out ipAddress))
+            {
+                //valid ip
+            }
+            else
+            {
+                //is not valid ip
+            }
+        }
+
+    }
+}
