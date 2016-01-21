c77662cd6fb1fdcb1929f9f1c9df8027c54e1c32
DenisBellottiMC <denis.bellotti@mcengineering.eu>
2016-01-21 15:39:12 +0100

    A complete asynchronous TCP listener is added
    This listener is based on the ReceiveTCP method

--- SocketTCPFilesExchange/frmSendFile.cs
+++ SocketTCPFilesExchange/frmSendFile.cs
@@ -6,7 +6,9 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,12 +16,85 @@ namespace SocketTCPFilesExchange
 {
     public partial class frmSendFile : Form
     {
+        const int BufferSize = 1024;
+        const int ServiceStandardPort = 999;
+
         public void ReceiveTCP(int portN)
-        { }
+        {
+            TcpListener Listener = null;
+            String Status;
+            try
+            {
+                Listener = new TcpListener(IPAddress.Any, portN);
+                Listener.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            byte[] RecData = new byte[BufferSize];
+            int RecBytes;
+
+            for (; ; )
+            {
+                TcpClient client = null;
+                NetworkStream netstream = null;
+                Status = string.Empty;
+                try
+                {
+                    string message = "Accept the Incoming File ";
+                    string caption = "Incoming Connection";
+                    MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                    DialogResult result;
+
+                    if (Listener.Pending())
+                    {
+                        client = Listener.AcceptTcpClient();
+                        netstream = client.GetStream();
+                        Status = "Connected to a client\n";
+                        result = MessageBox.Show(message, caption, buttons);
+
+                        if (result == System.Windows.Forms.DialogResult.Yes)
+                        {
+                            string SaveFileName = string.Empty;
+                            SaveFileDialog DialogSave = new SaveFileDialog();
+                            DialogSave.Filter = "All files (*.*)|*.*";
+                            DialogSave.RestoreDirectory = true;
+                            DialogSave.Title = "Where do you want to save the file?";
+                            DialogSave.InitialDirectory = @"C:/";
+                            if (DialogSave.ShowDialog() == DialogResult.OK)
+                                SaveFileName = DialogSave.FileName;
+                            if (SaveFileName != string.Empty)
+                            {
+                                int totalrecbytes = 0;
+                                FileStream Fs = new FileStream
+                 (SaveFileName, FileMode.OpenOrCreate, FileAccess.Write);
+                                while ((RecBytes = netstream.Read
+                     (RecData, 0, RecData.Length)) > 0)
+                                {
+                                    Fs.Write(RecData, 0, RecBytes);
+                                    totalrecbytes += RecBytes;
+                                }
+                                Fs.Close();
+                            }
+                            netstream.Close();
+                            client.Close();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    //netstream.Close();
+                }
+            }
+        }
 
         public frmSendFile()
         {
             InitializeComponent();
+            new Thread(delegate() { ReceiveTCP(ServiceStandardPort); });
         }
 
         private void btnSend_Click(object sender, EventArgs e)
@@ -27,7 +102,8 @@ namespace SocketTCPFilesExchange
             IPAddress ipAddress;
             if (IPAddress.TryParse(txtIPAddress.Text, out ipAddress))
             {
-                //valid ip
+
+                
             }
             else
             {
